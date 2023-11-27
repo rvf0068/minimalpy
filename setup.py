b8a1438cf5e21c947669b439f0dd8444af2e049b
Rafael Villarroel <rvf0068@gmail.com>
2023-11-27 12:34:44 -0600

    First commit
    A small Python package that can be installed by pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='minimal',
+      version='0.1',
+      description='A Python package for something',
+      url='http://github.com/rvf0068/minimal',
+      author='Yo Mero, Juan Pérez',
+      author_email='yomero@gmail.com, juanperez@gmail.com',
+      packages=['minimal'],
+      zip_safe=False)
